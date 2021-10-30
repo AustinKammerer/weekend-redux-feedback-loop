24ddc14957257a038f3c86df89b77c54c34bc1a1
Austin Kammerer <austin4191@gmail.com>
2021-10-30 11:05:26 -0500

    FeelingForm component dispatches form input to store

---
+++ src/components/FeelingForm/FeelingForm.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+
+export default function FeelingForm() {
+  // local state to store user input
+  const [feelingFeedback, setFeelingFeedback] = useState("");
+
+  const dispatch = useDispatch();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch({ type: "ADD_FEELING", payload: feelingFeedback });
+  };
+  return (
+    <>
+      <h2>How are you feeling today?</h2>
+      <form onSubmit={handleSubmit}>
+        <input
+          value={feelingFeedback}
+          type="text"
+          id="feelingFeedback"
+          name="feeling"
+          placeholder="feeling"
+          onChange={(e) => setFeelingFeedback(e.target.value)}
+        />
+        <button type="submit">NEXT</button>
+      </form>
+    </>
+  );
+}
